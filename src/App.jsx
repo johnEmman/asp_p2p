d3256655263614f7912a271bc00dc503a796e4c6
john emman <emmanuel11052002@gmail.com>
2024-11-05 23:52:16 +0800

    lezzgoo!!

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SpeechToTextTranscriber from "./components/SpeechToTextTranscriber";
+export default function App() {
+  return (
+    <div>
+      <SpeechToTextTranscriber />
+    </div>
+  );
+}
